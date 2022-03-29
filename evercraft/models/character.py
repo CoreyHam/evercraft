65f9c2207c0323d6671e602df627d9fdd1a72058
Corey <corey.hampton@student.awesomeinc.org>
2022-03-29 18:13:51 +0000

    Key value sets working

--- evercraft/models/character.py
+++ evercraft/models/character.py
@@ -1,3 +1,24 @@
+import pytest
+
+# def func(x):
+#     return x + 1
+
+# def func2(x):
+#     return x * 2
+
+
 # this is where your character code will go
 
-        
+class Character():
+    def __init__(self):
+        self.values = {
+            "key" : "value",
+            "name" : "default",
+            "alignment" : "default"
+        }
+
+    def set_name(self, name):
+        self.values["name"] = name
+    
+    def set_alignment(self, alignment):
+        self.values["alignment"] = alignment
---
+++ tests/test_iteration_1/test_1_create_a_character.py
@@ -0,0 +1,43 @@
+import pytest
+from evercraft.models.character import Character
+
+# def test_answer():
+#     assert func(4) == 5
+
+# def test_whatever():
+#     assert func2(4) == 8
+
+
+# Make sure we have access to Character class
+def test_CreateChar():
+    c = Character()
+    assert isinstance(c, Character)
+
+# Set character name
+def test_CreateCharName():
+    c = Character()
+    c.set_name("Billy")
+    assert c.values["name"] == "Billy"
+
+# Two Characters
+def test_CreateTwoCharName():
+    c1 = Character()
+    c1.set_name("Billy")
+    c2 = Character()
+    c2.set_name("Jeff")
+    assert c1.values["name"] == "Billy" and c2.values["name"] == "Jeff"
+
+def test_CreateAlignment():
+    c = Character()
+    c.set_alignment("Good")
+    assert c.values["alignment"] == "Good"
+    
+
+def test_CreateTwoAlignment():
+    c1 = Character()
+    c1.set_alignment("Good")
+    assert c1.values["alignment"] == "Good"
+    c2 = Character()
+    c2.set_alignment("Evil")
+    assert c2.values["alignment"] == "Evil"
+    
